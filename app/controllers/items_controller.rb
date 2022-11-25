5187a54b946d1a98b586917727b9cf50b97373ba
yurika0705 <116805970+yurika0705@users.noreply.github.com>
2022-11-25 16:31:27 +0900

    商品一覧機能
    This reverts commit 0330fc0d42823ed3c8270439049eb82a89f73465.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
+
   def index
-    # @items = Item.includes(:user).order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
